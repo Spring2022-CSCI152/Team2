cdb7a77d82156fc585fd6e780fd42b6703da3046
John Eagan <johnjeagan@mail.fresnostate.edu>
2022-02-09 00:10:03 -0800

    routing structure

--- frontend/src/Navbar.jsx
+++ frontend/src/Navbar.jsx
@@ -2,13 +2,16 @@ import React from 'react';
 import './Navbar.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faUser, faSearch } from '@fortawesome/free-solid-svg-icons';
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
     return ( 
         <div className="navContainer">
             <div className="leftSide">
                 <div className="logoContainer">
-                    <img src="/logo192.png" alt="logo" className="logo"/>
+                    <Link to="/" className="homeButton">
+                        <img src="/logo192.png" alt="logo" className="logo"/>
+                    </Link>
                 </div>
                 <div className="searchBar">
                     <input type="text" placeholder="Search" className="searchInput"/>
@@ -19,8 +22,10 @@ const Navbar = () => {
                 </div>
             </div>
             <div className="rightSide">
-                <button className="aboutButton"> About </button>
-                <button className="loginButton"> Login </button>
+                <Link to="/about" className="aboutButton"> About </Link>
+                <Link to="/login" className="loginButton"> Login </Link>
+                {/* <button className="aboutButton"> About </button> */}
+                {/* <button className="loginButton"> Login </button> */}
                 <div className="profilePictureContainer">
                     <FontAwesomeIcon icon={faUser} />
                     {/* <img src="/placeholderPFP.png" alt="profile" className="profilePicture"/> */}
