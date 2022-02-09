cf7de5ba36ffe661cb6cbc621447195ac082eb48
John Eagan <johnjeagan@mail.fresnostate.edu>
2022-02-08 23:31:52 -0800

    navbar styling and fontawesome inclusion

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+
 
 import Navbar from './Navbar.jsx';
 
 function App() {
+
   return (
     <div className="App">
       <Navbar />
+      <header className="App-header">
+        
+        <img src={logo} className="App-logo" alt="logo" />
+        <p> Yo {0} </p>
+      </header>
     </div>
   );
 }
--- frontend/src/Navbar.jsx
+++ frontend/src/Navbar.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import './Navbar.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUser, faSearch } from '@fortawesome/free-solid-svg-icons';
 
 const Navbar = () => {
     return ( 
@@ -10,13 +12,18 @@ const Navbar = () => {
                 </div>
                 <div className="searchBar">
                     <input type="text" placeholder="Search" className="searchInput"/>
+                    <button className="searchButton">
+                        <FontAwesomeIcon icon={faSearch} />
+                        {/* <img src="/search.png" alt="searchImage" height="100%"/> */}
+                    </button>
                 </div>
             </div>
             <div className="rightSide">
                 <button className="aboutButton"> About </button>
                 <button className="loginButton"> Login </button>
                 <div className="profilePictureContainer">
-                    <img src="/placeholderPFP.png" alt="profile" className="profilePicture"/>
+                    <FontAwesomeIcon icon={faUser} />
+                    {/* <img src="/placeholderPFP.png" alt="profile" className="profilePicture"/> */}
                 </div>
             </div>
         </div>
