c6499594f998406e4c419a416a45b37b60566f8a
John Eagan <johnjeagan@mail.fresnostate.edu>
2022-02-16 09:52:41 -0800

    fixing css import

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faGithub, faDiscord } from '@fortawesome/free-brands-svg-icons';
 
-import './Footer.css';
+import '../Footer.css';
 
 const Footer = () => {
     return (
