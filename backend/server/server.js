6da3e0a2e8ab53326b487326f30d878a75787d72
Mitchell Agcaoili Maltezo <81717745+maltezoa@users.noreply.github.com>
2022-02-19 20:50:14 -0800

    Update server.js
    - tested mongodb connection

--- backend/server/server.js
+++ backend/server/server.js
@@ -2,11 +2,21 @@ require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
+const mongoose = require('mongoose');
 const port = process.env.PORT || 5000;
 
 // body parser to get data from POST requests
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+// Connection to Mongo
+mongoose.connect(process.env.DB, {useNewUrlParser: true});
+    mongoose.connection.once('open', function(){
+      console.log('Successfully connected to MongoDB');
+    }).on('error', function(error){
+        console.log('Error is: ', error);
+    });
+
 
 /* // Use API routes from the api folder (un-comment this once we have an api ready
 const apis = require("./api");
