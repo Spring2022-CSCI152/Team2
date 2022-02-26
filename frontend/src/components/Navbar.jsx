6d1aa8d8473813f606353fd23ab363de00951225
AnthonyMarin <90010906+AnthonyMarin@users.noreply.github.com>
2022-02-26 15:28:52 -0800

    changed alert to bell

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import '../assets/NavBar.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faUser, faSearch, faBars } from '@fortawesome/free-solid-svg-icons';
+import { faUser, faSearch, faBars, faBell } from '@fortawesome/free-solid-svg-icons';
 import { Link } from "react-router-dom";
 import { useMediaQuery } from 'react-responsive';
 
@@ -31,7 +31,7 @@ const Navbar = () => {
                     <React.Fragment>
                         <Link to={"/about"} className="aboutButton"> About </Link>
                         <Link to={"/login"} className="loginButton"> Login </Link>
-                        <Link to={"/alerts/"} className="alertsButton"> Alerts </Link>
+                        
 
                     </React.Fragment>
                 }
@@ -40,7 +40,13 @@ const Navbar = () => {
                         <FontAwesomeIcon icon={faBars} className="hamburger" />
                     </React.Fragment>
                 }
+                <div className="alertsContainer">
+               
+                <Link to={"/alerts/"} className=""> <FontAwesomeIcon icon={faBell} />  </Link>
+                    {/* <img src="/placeholderPFP.png" alt="profile" className="profilePicture"/> */}
+                </div>
                 <div className="profilePictureContainer">
+                
                     <FontAwesomeIcon icon={faUser} />
                     {/* <img src="/placeholderPFP.png" alt="profile" className="profilePicture"/> */}
                 </div>
