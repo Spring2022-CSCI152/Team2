cae1ffa6724338cbe6dc4317ab5fd3af3d2a2d91
Mitchell Agcaoili Maltezo <81717745+maltezoa@users.noreply.github.com>
2022-03-06 22:53:24 -0800

    added passport code/dependencies

--- backend/config/passport.js
+++ backend/config/passport.js
@@ -1,1 +1,24 @@
-// some code
+require('dotenv').config();
+const jwtStrat = require('passport-jwt').Strategy;
+const extractJWT = require('passport-jwt').ExtractJwt;
+const mongoose = require('mongoose');
+const User = mongoose.model('users');
+
+const opts = {};
+opts.jwtFromRequest = extractJWT.fromAuthHeaderAsBearerToken();
+opts.secretKey = process.env.secretKey;
+
+module.exports = passport => {
+    passport.use(
+        new jwtStrat(opts, (jwt_payload, done) =>{
+            User.findById(jwt_payload.id)
+                .then(user => {
+                    if (user){
+                        return done(null,user);
+                    }
+                    return done(null,false);
+                })
+                .catch(err => console.log(err));
+        })
+    );
+};
