af1b44056f3b39040b72ef91b6c88b1b6cc5b7e3
Mary Valencia <80990614+MaryValencia0220@users.noreply.github.com>
2022-03-24 17:30:54 -0700

    Account Page
    ImageList

---
+++ frontend/src/components/ImageList.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+// Rendering individual images
+const Image = ({ image }) => {
+  return (
+    <div className="file-item">
+      <img alt={`img - ${image.id}`} src={image.src} className="file-img" />
+    </div>
+  );
+};
+
+// ImageList Component
+const ImageList = ({ images }) => {
+  
+  // render each image by calling Image component
+  const renderImage = (image, index) => {
+    return (
+      <Image
+        image={image}
+        key={`${image.id}-image`}
+      />
+    );
+  };
+
+  // Return the list of files
+  return <section className="file-list">{images.map(renderImage)}</section>;
+};
+
+export default ImageList;
