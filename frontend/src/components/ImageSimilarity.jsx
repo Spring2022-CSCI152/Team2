1cd920842b6f45154e0a97b7e9777a420f892871
John Eagan <johnjeagan@mail.fresnostate.edu>
2022-03-29 17:47:58 -0700

    image upload complete

--- frontend/src/components/ImageSimilarity.jsx
+++ frontend/src/components/ImageSimilarity.jsx
@@ -1,31 +1,34 @@
-import React, { Component } from 'react';
-import S3FileUpload from 'react-s3';
+import React, { Component, useState } from 'react';
+import { S3FileUpload, uploadFile } from 'react-s3';
 import Buffer from 'buffer';
 window.Buffer = window.Buffer || require("buffer").Buffer;
 
 const config = {
     bucketName: 'notforthieves',
     region: 'us-west-1',
-    accessKeyId: process.env.S3_ACCESS_KEY_ID,
-    secretAccessKey: process.env.S3_SECRET_ACCESS_KEY
-}
-
-const uploadFile = (e) => {
-    S3FileUpload.uploadFile(e.target.files[0], config)
-    .then(data => console.log(data.location))
-    .catch(err => console.error(err))
+    accessKeyId: process.env.REACT_APP_S3_ACCESS_KEY_ID,
+    secretAccessKey: process.env.REACT_APP_S3_SECRET_ACCESS_KEY
 }
 
 const ImageSimilarity = () => {
+    const [selectedFile, setSelectedFile] = useState(null);
+
+    const handleFileInput = (e) => {
+        setSelectedFile(e.target.files[0]);
+    }
+
+    const handleUpload = async (file) => {
+        console.log(config);
+        uploadFile(file, config)
+            .then(data => console.log(data))
+            .catch(err => console.error(err))
+    }
+
     return ( 
         <div className="imageSimilarity">
             <div className="imageSimilarityContainer">
-                <input type="file" onChange={ uploadFile } />
-
-                {/* <form onSubmit={uploadFile}>
-                    <input type="file"/>
-                    <input type="submit"></input>
-                </form> */}
+                <input type="file" onChange={ handleFileInput } />
+                <button onClick={ () => handleUpload(selectedFile) }>Upload</button>
             </div>
         </div>
     );
