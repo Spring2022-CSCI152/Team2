769ca71b79d08b3692ccd22d2ef5dc50f53b927f
tquigg96 <t.quigg.hulsey@outlook.com>
2022-04-08 17:41:23 -0700

    moved logic to controller

--- backend/routes/collections.js
+++ backend/routes/collections.js
@@ -1,20 +1,12 @@
 const express = require('express');
-const { collection } = require('../models/collections');
 const router = express.Router();
-const collections = require('../models/collections');
-const validateCollectionsInput = require('../validation/collections');
+const {collections} = require('../controller/collectionController');
 
-router.post('/collections', (req, res) => {
-    // Some code
 
-    const { errors, isValid } = validateCollectionsInput(req.body);
+router.post('/', collections)
 
-    // Check if valid
-    if(!isValid){
-        return res.status(400).json(errors);
-    }
+module.exports = router;
 
 
 
  
-});
